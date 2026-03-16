dbea8e5801cbfd120bc11af0ff821a9bf12060dc
AngadAhluwalia <angadahluwalia2005@gmail.com>
2026-03-15 23:15:42 -0400

    Update leetcode.java with changes on 2026-03-15

--- Desktop/ASS2_ANGADAHLUWALIA_M/leetcode.java
+++ Desktop/ASS2_ANGADAHLUWALIA_M/leetcode.java
@@ -1,4 +1,5 @@
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class leetcode {
@@ -38,6 +39,15 @@ public class leetcode {
 
         
     }
-
+    // 1. two sum
+   public int[] twoSum(int[] nums, int target){
+    HashMap<Integer,Integer> bla= new HashMap<>();
+    for(int i=0;i<nums.length;i++){
+        int complement= target-nums[i];
+        if(bla.containsKey(complement)) {return new int[]{bla.get(complement), i};}
+        bla.put(nums[i], i);
+    }
+    return new int[]{0,0};
+   } 
 
 }
